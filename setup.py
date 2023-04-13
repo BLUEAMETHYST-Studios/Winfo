6d679f0685e66d91de1ee4ddc4d30f37b6b05eef
Simoso68 <87707341+Simoso68@users.noreply.github.com>
2023-04-13 21:33:57 +0200

    0.0.3.1

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as f:
     
 setup(
     name="Winfo",
-    version="0.0.3",
+    version="0.0.3.1",
     author="BLUEAMETHYST Studios",
     description="Get information about your windows system",
     long_description_content_type="text/markdown",
